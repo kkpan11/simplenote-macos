1884fed8218eab8188e014ca17e53baa1cea4b23
Charlie Scheer <charlie.scheer@automattic.com>
2024-04-12 14:47:32 -0600

    Refactored management of creating windows for notes

--- Simplenote/NoteWindow.swift
+++ Simplenote/NoteWindow.swift
@@ -9,6 +9,27 @@
 import Foundation
 
 class NoteWindow: NSWindow {
+    private let editor: NoteEditorViewController
+
+    init() {
+        let storyboard = NSStoryboard(name: .main, bundle: nil)
+        self.editor = storyboard.instantiateViewController(ofType: NoteEditorViewController.self)
+
+        let rect = NSRect(x: 100, y: 100, width: 100, height: 100)
+        super.init(contentRect: rect, styleMask: [.titled, .closable, .miniaturizable, .resizable], backing: .buffered, defer: false)
+
+        setupWindow()
+    }
+
+    private func setupWindow() {
+        contentViewController = editor
+
+        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        let fileURL = URL(fileURLWithPath: documentsDirectory, isDirectory: true).appendingPathComponent(".editor-metadata-cache")
+        let editorCache = NoteEditorMetadataCache(storage: FileStorage(fileURL: fileURL))
+        editor.metadataCache = editorCache
+    }
+
     // The note windows are stored in the windows manager and need to be removed when they close
     // This override cleans up the windows in the manager
     override func close() {
@@ -18,5 +39,14 @@ class NoteWindow: NSWindow {
            let index = noteWindowsManager.windowControllers.firstIndex(of: controller) {
             noteWindowsManager.windowControllers.remove(at: index)
         }
+    }
+
+
+    //MARK: Show Note
+    //
+    func show(_ note: Note) {
+        editor.toolbarView.sidebarButton.isHidden = true
+        editor.displayNote(note)
+        title = note.titlePreview
     }
 }
--- Simplenote/NoteWindowController.swift
+++ Simplenote/NoteWindowController.swift
@@ -9,24 +9,9 @@
 import Foundation
 
 class NoteWindowController: NSWindowController {
-    private let editor: NoteEditorViewController
-    private let editorCache: NoteEditorMetadataCache
 
     init() {
-        let storyboard = NSStoryboard(name: .main, bundle: nil)
-        let noteEditor = storyboard.instantiateViewController(ofType: NoteEditorViewController.self)
-
-        let documentsDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-        let fileURL = URL(fileURLWithPath: documentsDirectory, isDirectory: true).appendingPathComponent(".editor-metadata-cache")
-        self.editorCache = NoteEditorMetadataCache(storage: FileStorage(fileURL: fileURL))
-
-        noteEditor.metadataCache = editorCache
-
-        let window = NoteWindow(contentViewController: noteEditor)
-
-        self.editor = noteEditor
-
-        super.init(window: window)
+        super.init(window: NoteWindow())
     }
 
     required init?(coder: NSCoder) {
@@ -34,10 +19,7 @@ class NoteWindowController: NSWindowController {
     }
 
     func show(_ note: Note) {
-        editor.toolbarView.sidebarButton.isHidden = true
-        editor.displayNote(note)
-        window?.title = note.titlePreview
-
+        (window as? NoteWindow)?.show(note)
         showWindow(window)
     }
 }
