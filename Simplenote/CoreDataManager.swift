b2b636c3e0d43d80cf9da36dc38096a3c3be2071
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-23 14:26:04 -0600

    Added initializing to the core data manager

--- Simplenote/CoreDataManager.swift
+++ Simplenote/CoreDataManager.swift
@@ -15,9 +15,61 @@ class CoreDataManager: NSObject {
     private(set) var managedObjectContext: NSManagedObjectContext
     private(set) var persistentStoreCoordinator: NSPersistentStoreCoordinator
 
-    init(managedObjectModel: NSManagedObjectModel, managedObjectContext: NSManagedObjectContext, persistentStoreCoordinator: NSPersistentStoreCoordinator) {
-        self.managedObjectModel = managedObjectModel
-        self.managedObjectContext = managedObjectContext
-        self.persistentStoreCoordinator = persistentStoreCoordinator
+    init(storageSettings: StorageSettings = StorageSettings()) throws {
+        guard let modelURL = storageSettings.modelURL,
+              let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            throw NSError(domain: "CoreDataManager", code: 1)
+        }
+
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        let psc = try Self.preparePSC(with: storageSettings, model: mom)
+
+        self.managedObjectModel = mom
+        self.managedObjectContext = context
+        self.persistentStoreCoordinator = psc
+    }
+
+    static private func preparePSC(with storageSettings: StorageSettings, model: NSManagedObjectModel) throws -> NSPersistentStoreCoordinator {
+        guard let applicationFilesDirectory = storageSettings.applicationFilesDirectory else {
+            throw NSError(domain: "CoreDataManager", code: 1)
+        }
+
+        // Validate the directory for the store DB
+        do {
+            try Self.validateResourceValueForDirectory(at: applicationFilesDirectory)
+        } catch {
+            try handleDirectoryError((error as NSError), directoryURL: applicationFilesDirectory)
+        }
+
+
+        let options = [
+            NSMigratePersistentStoresAutomaticallyOption: true,
+            NSInferMappingModelAutomaticallyOption: true
+        ]
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+
+        try coordinator.addPersistentStore(ofType: NSXMLStoreType, configurationName: nil, at: storageSettings.storageURL, options: options)
+
+        return coordinator
+    }
+
+    static private func validateResourceValueForDirectory(at url: URL) throws {
+        let properties = try url.resourceValues(forKeys: [URLResourceKey.isDirectoryKey])
+
+        if properties.isDirectory != true {
+            let failureDescription = String(format: "Expected a folder to store application data, found a file (%@).", url.path)
+            var dict: [String: Any] = [:]
+            dict[NSLocalizedDescriptionKey] = failureDescription
+            let error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 101, userInfo: dict)
+            throw error
+        }
+    }
+
+    static private func handleDirectoryError(_ error: NSError, directoryURL: URL) throws {
+        if error.code == NSFileReadNoSuchFileError {
+            try FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+        } else {
+            throw error
+        }
     }
 }
---
+++ Simplenote/StorageSettings.swift
@@ -0,0 +1,36 @@
+//
+//  StorageSettings.swift
+//  Simplenote
+//
+//  Created by Charlie Scheer on 5/23/24.
+//  Copyright © 2024 Simperium. All rights reserved.
+//
+
+import Foundation
+
+class StorageSettings {
+    let fileManager: FileManager
+
+    init(fileManger: FileManager = .default) {
+        self.fileManager = fileManger
+    }
+
+    var applicationFilesDirectory: URL? {
+        let libraryURL = fileManager.urls(for: .libraryDirectory, in: .userDomainMask).last
+        return libraryURL?.appendingPathComponent(Constants.modelName)
+    }
+
+    var modelURL: URL? {
+        Bundle.main.url(forResource: Constants.modelName, withExtension: Constants.modelExtension)
+    }
+
+    var storageURL: URL? {
+        applicationFilesDirectory?.appendingPathComponent("\(Constants.modelName).\(Constants.storeExtension)")
+    }
+}
+
+private struct Constants {
+    static let modelName = "Simplenote"
+    static let modelExtension = "momd"
+    static let storeExtension = "storedata"
+}
