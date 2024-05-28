cbf9d1a612af640086301100d38eb4f61330de50
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-28 16:46:07 -0600

    Added access to the root bundle identifier

--- Simplenote/Bundle+Simplenote.swift
+++ Simplenote/Bundle+Simplenote.swift
@@ -11,4 +11,49 @@ extension Bundle {
         let version = infoDictionary?["CFBundleShortVersionString"] as? String
         return version ?? ""
     }
+
+    var rootBundleIdentifier: String {
+        guard isAppExtensionBundle,
+              bundleURL.path.components(separatedBy: "/").contains(where: { $0.hasSuffix(Constants.appSuffix) }) else {
+            return bundleIdentifier ?? Constants.defaultBundleID
+        }
+
+        var url = bundleURL
+        while !url.lastPathComponent.hasSuffix(Constants.appSuffix) {
+            url.deleteLastPathComponent()
+        }
+
+        return (Bundle(url: url)?.object(forInfoDictionaryKey: kCFBundleIdentifierKey as String) as? String) ?? Constants.defaultBundleID
+    }
+    /*
+     static let sharedGroupDomain = "\(Bundle.main.teamIDPrefix ?? "PZYM8XX95Q")group.\(Bundle.main.rootBundleIdentifier ?? "com.codality.NotationalFlow")
+
+     var teamIDPrefix: String? {
+         object(forInfoDictionaryKey: Constants.teamIDPrefix) as? String
+     }
+
+     */
+}
+
+private extension Bundle {
+    var isAppExtensionBundle: Bool {
+    #if APP_EXTENSION
+        return true
+    #else
+        return false
+    #endif
+    }
+}
+
+
+private struct Constants {
+    static let teamIDPrefix = "TeamIDPrefix"
+    static let appSuffix = ".app"
+    static let defaultBundleID: String  = {
+        var id = "com.automattic.SimplenoteMac"
+        #if DEBUG
+        id.append(".Debug")
+        #endif
+        return id
+    }()
 }
---
+++ Simplenote/FileManager+Simplenote.swift
@@ -0,0 +1,9 @@
+//
+//  FileManager+Simplenote.swift
+//  Simplenote
+//
+//  Created by Charlie Scheer on 5/28/24.
+//  Copyright © 2024 Simperium. All rights reserved.
+//
+
+import Foundation
