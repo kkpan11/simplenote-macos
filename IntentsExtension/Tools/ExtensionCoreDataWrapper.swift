867dabecb6dc359d6c203b2bc21f9aa9f156892e
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-29 11:59:20 -0600

    Added extension core data manager to intents

---
+++ IntentsExtension/Tools/ExtensionCoreDataWrapper.swift
@@ -0,0 +1,35 @@
+//
+//  ExtensionCoreDataWrapper.swift
+//  IntentsExtension
+//
+//  Created by Charlie Scheer on 5/29/24.
+//  Copyright © 2024 Simperium. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ExtensionCoreDataWrapper {
+    private lazy var coreDataManager: CoreDataManager = {
+        do {
+            return try CoreDataManager(at: StorageSettings().sharedStorageURL, for: .intents)
+        } catch {
+            fatalError()
+        }
+    }()
+
+    private lazy var extensionResultsController: ExtensionResultsController = {
+        ExtensionResultsController(context: coreDataManager.managedObjectContext)
+    }()
+
+    func resultsController() -> ExtensionResultsController? {
+        guard FileManager.default.fileExists(atPath: StorageSettings().sharedStorageURL.path) else {
+            return nil
+        }
+        return extensionResultsController
+    }
+
+    func context() -> NSManagedObjectContext {
+        coreDataManager.managedObjectContext
+    }
+}
