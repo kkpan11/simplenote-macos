7c90d96813c02e1f57a73d74298601b5194d8548
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-31 14:59:14 -0600

    Added new files to help fetch tags for intents

---
+++ IntentsExtension/IntentHandlers/FindNoteWithTagIntentHandler.swift
@@ -0,0 +1,9 @@
+//
+//  FindNoteWithTagIntentHandler.swift
+//  IntentsExtension
+//
+//  Created by Charlie Scheer on 5/31/24.
+//  Copyright © 2024 Simperium. All rights reserved.
+//
+
+import Foundation
