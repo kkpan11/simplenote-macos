24e651f6a3c49d1ac993525b3d3d6eb2689fc802
Charlie Scheer <charlie.scheer@automattic.com>
2024-05-31 15:07:07 -0600

    Added handler method for find note with tag

--- IntentsExtension/IntentHandlers/FindNoteWithTagIntentHandler.swift
+++ IntentsExtension/IntentHandlers/FindNoteWithTagIntentHandler.swift
@@ -27,35 +27,13 @@ class FindNoteWithTagIntentHandler: NSObject, FindNoteWithTagIntentHandling {
     }
 
     func handle(intent: FindNoteWithTagIntent, completion: @escaping (FindNoteWithTagIntentResponse) -> Void) {
-        
+        guard let note = intent.note else {
+            completion(FindNoteWithTagIntentResponse(code: .failure, userActivity: nil))
+            return
+        }
+
+        let response = FindNoteWithTagIntentResponse(code: .success, userActivity: nil)
+        response.note = note
+        completion(response)
     }
 }
-
-/*
- func resolveNote(for intent: FindNoteWithTagIntent) async -> IntentNoteResolutionResult {
-     if let selectedNote = intent.note {
-         return IntentNoteResolutionResult.success(with: selectedNote)
-     }
-
-     guard let selectedTag = intent.tag else {
-         return IntentNoteResolutionResult.needsValue()
-     }
-
-     return IntentNoteResolutionResult.resolveIntentNote(forTag: selectedTag, in: coreDataWrapper)
- }
-
- func provideTagOptionsCollection(for intent: FindNoteWithTagIntent) async throws -> INObjectCollection<IntentTag> {
-     let tags = try IntentTag.allTags(in: coreDataWrapper)
-     return INObjectCollection(items: tags)
- }
-
- func handle(intent: FindNoteWithTagIntent) async -> FindNoteWithTagIntentResponse {
-     guard let note = intent.note else {
-         return FindNoteWithTagIntentResponse(code: .failure, userActivity: nil)
-     }
-
-     let response = FindNoteWithTagIntentResponse(code: .success, userActivity: nil)
-     response.note = note
-     return response
- }
- */
