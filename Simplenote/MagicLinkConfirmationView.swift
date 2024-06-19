bf1d8408339ed276d1447db833507a9581ba1d64
Jorge Leandro Perez <jorge@lantean.co>
2024-06-19 17:39:32 -0300

    Import MagicLinkConfirmationView

---
+++ Simplenote/MagicLinkConfirmationView.swift
@@ -0,0 +1,60 @@
+import Foundation
+import SwiftUI
+
+
+// MARK: - MagicLinkConfirmationView
+//
+struct MagicLinkConfirmationView: View {
+    
+    @State var displaysInvalidLink = false
+    
+    var onDismissRequest: (() -> Void)?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            if displaysInvalidLink {
+                invalidLinkText
+                invalidLinkButton
+            } else {
+                authorizingText
+                progressIndicator
+            }
+        }
+        .padding()
+        .background(.white)
+        .frame(width: 380, height: 200, alignment: .center)
+        .onReceive(NotificationCenter.default.publisher(for: .magicLinkAuthDidFail)) { _ in
+            displaysInvalidLink = true
+        }
+    }
+    
+    private var authorizingText: some View {
+        Text("Logging In...")
+            .font(.title3)
+            .foregroundColor(.gray)
+    }
+    
+    private var progressIndicator: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle())
+            .controlSize(.small)
+    }
+    
+    private var invalidLinkText: some View {
+        Text("Link was no longer valid")
+            .font(.title3)
+            .foregroundColor(.gray)
+    }
+    
+    private var invalidLinkButton: some View {
+        SwiftUI.Button(action: switchToAuthenticationUI) {
+            Text("Accept")
+                .fontWeight(.bold)
+                .cornerRadius(5)
+        }
+    }
+    
+    func switchToAuthenticationUI() {
+        onDismissRequest?()
+    }
+}
