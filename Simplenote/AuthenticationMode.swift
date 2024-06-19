f3bb90bbce0af2d1e0cc4e9d29553401e1e58471
Jorge Leandro Perez <jorge@lantean.co>
2024-06-19 13:32:21 -0300

    AuthenticationMode: Adds Animation Text property

--- Simplenote/AuthenticationMode.swift
+++ Simplenote/AuthenticationMode.swift
@@ -5,6 +5,7 @@ import Foundation
 //
 class AuthenticationMode: NSObject {
     let primaryActionText: String
+    let primaryActionAnimationText: String
     let primaryActionSelector: Selector
     
     let secondaryActionText: String?
@@ -18,8 +19,9 @@ class AuthenticationMode: NSObject {
     let isSecondaryActionVisible: Bool
     let isWordPressVisible: Bool
     
-    init(primaryActionText: String, primaryActionSelector: Selector, secondaryActionText: String?, secondaryActionSelector: Selector?, switchActionText: String, switchActionTip: String, switchTargetMode: @escaping () -> AuthenticationMode, isPasswordVisible: Bool, isSecondaryActionVisible: Bool, isWordPressVisible: Bool) {
+    init(primaryActionText: String, primaryActionAnimationText: String, primaryActionSelector: Selector, secondaryActionText: String?, secondaryActionSelector: Selector?, switchActionText: String, switchActionTip: String, switchTargetMode: @escaping () -> AuthenticationMode, isPasswordVisible: Bool, isSecondaryActionVisible: Bool, isWordPressVisible: Bool) {
         self.primaryActionText = primaryActionText
+        self.primaryActionAnimationText =  primaryActionAnimationText
         self.primaryActionSelector = primaryActionSelector
         self.secondaryActionText = secondaryActionText
         self.secondaryActionSelector = secondaryActionSelector
@@ -76,6 +78,7 @@ extension AuthenticationMode {
     @objc
     static var loginWithPassword: AuthenticationMode {
         AuthenticationMode(primaryActionText: LoginStrings.primaryAction,
+                           primaryActionAnimationText: LoginStrings.primaryAnimationText,
                            primaryActionSelector: #selector(AuthViewController.pressedLogInWithPassword),
                            secondaryActionText: LoginStrings.secondaryAction,
                            secondaryActionSelector: #selector(AuthViewController.openForgotPasswordURL),
@@ -92,6 +95,7 @@ extension AuthenticationMode {
     @objc
     static var loginWithMagicLink: AuthenticationMode {
         AuthenticationMode(primaryActionText: MagicLinkStrings.primaryAction,
+                           primaryActionAnimationText: MagicLinkStrings.primaryAnimationText,
                            primaryActionSelector: #selector(AuthViewController.pressedLoginWithMagicLink),
                            secondaryActionText: MagicLinkStrings.secondaryAction,
                            secondaryActionSelector: #selector(AuthViewController.switchToPasswordAuth),
@@ -108,6 +112,7 @@ extension AuthenticationMode {
     @objc
     static var signup: AuthenticationMode {
         AuthenticationMode(primaryActionText: SignupStrings.primaryAction,
+                           primaryActionAnimationText: SignupStrings.primaryAnimationText,
                            primaryActionSelector: #selector(AuthViewController.pressedSignUp),
                            secondaryActionText: nil,
                            secondaryActionSelector: nil,
@@ -124,21 +129,24 @@ extension AuthenticationMode {
 // MARK: - Localization
 //
 private enum LoginStrings {
-    static let primaryAction    = NSLocalizedString("Log In", comment: "Title of button for logging in")
-    static let secondaryAction  = NSLocalizedString("Forgot your Password?", comment: "Forgot Password Button")
-    static let switchAction     = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
-    static let switchTip        = NSLocalizedString("Need an account?", comment: "Link to create an account")
+    static let primaryAction        = NSLocalizedString("Log In", comment: "Title of button for logging in")
+    static let primaryAnimationText = NSLocalizedString("Logging In...", comment: "Title of button for logging in")
+    static let secondaryAction      = NSLocalizedString("Forgot your Password?", comment: "Forgot Password Button")
+    static let switchAction         = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
+    static let switchTip            = NSLocalizedString("Need an account?", comment: "Link to create an account")
 }
 
 private enum MagicLinkStrings {
-    static let primaryAction    = NSLocalizedString("Instantly Log In with Email", comment: "Title of button for logging in")
-    static let secondaryAction  = NSLocalizedString("Continue with Password", comment: "Continue with Password Action")
-    static let switchAction     = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
-    static let switchTip        = NSLocalizedString("Need an account?", comment: "Link to create an account")
+    static let primaryAction        = NSLocalizedString("Instantly Log In with Email", comment: "Title of button for logging in")
+    static let primaryAnimationText = NSLocalizedString("Requesting Email...", comment: "Title of button for logging in")
+    static let secondaryAction      = NSLocalizedString("Continue with Password", comment: "Continue with Password Action")
+    static let switchAction         = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
+    static let switchTip            = NSLocalizedString("Need an account?", comment: "Link to create an account")
 }
 
 private enum SignupStrings {
-    static let primaryAction    = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
-    static let switchAction     = NSLocalizedString("Log In", comment: "Title of button for logging in up")
-    static let switchTip        = NSLocalizedString("Already have an account?", comment: "Link to sign in to an account")
+    static let primaryAction        = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
+    static let primaryAnimationText = NSLocalizedString("Signing Up...", comment: "Title of button for logging in")
+    static let switchAction         = NSLocalizedString("Log In", comment: "Title of button for logging in up")
+    static let switchTip            = NSLocalizedString("Already have an account?", comment: "Link to sign in to an account")
 }
