045295c0e5c58d80baf363cc7f40669380b91790
Charlie Scheer <charlie.scheer@automattic.com>
2024-07-26 09:29:40 -0600

    Updated text for login with email button

--- Simplenote/AuthenticationMode.swift
+++ Simplenote/AuthenticationMode.swift
@@ -199,7 +199,7 @@ private enum LoginStrings {
 }
 
 private enum MagicLinkStrings {
-    static let primaryAction        = NSLocalizedString("Instantly Log In with Email", comment: "Title of button for logging in")
+    static let primaryAction        = NSLocalizedString("Log in with email", comment: "Title of button for logging in")
     static let primaryAnimationText = NSLocalizedString("Requesting Email...", comment: "Title of button for logging in")
     static let secondaryAction      = NSLocalizedString("Continue with Password", comment: "Continue with Password Action")
     static let switchAction         = NSLocalizedString("Sign Up", comment: "Title of button for signing up")
